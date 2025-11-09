300fe922b6615580853d9e5b00ff5981764a9d34
dungs072 <82323846+dungs072@users.noreply.github.com>
2025-11-09 12:56:24 +0700

    Add filter block and split the color collections

--- Assets/Scripts/Constants/ColorEnum.cs
+++ Assets/Scripts/Constants/ColorEnum.cs
@@ -8,7 +8,8 @@ public enum ColorEnum
     YELLOW,
     CYAN,
     PURPLE,
-    WHITE
+    WHITE,
+    NONE
 }
 
 public static class ColorEnumExtensions
@@ -25,6 +26,20 @@ public static class ColorEnumExtensions
             ColorEnum.PURPLE => ProductID.PURPLE,
             ColorEnum.WHITE => ProductID.WHITE,
             _ => throw new System.ArgumentOutOfRangeException()
+        };
+    }
+    public static string ToHex(this ColorEnum color)
+    {
+        return color switch
+        {
+            ColorEnum.RED => "#FF0000",
+            ColorEnum.GREEN => "#00FF00",
+            ColorEnum.BLUE => "#0000FF",
+            ColorEnum.YELLOW => "#FFFF00",
+            ColorEnum.CYAN => "#00FFFF",
+            ColorEnum.PURPLE => "#800080",
+            ColorEnum.WHITE => "#FFFFFF",
+            _ => "#000000"
         };
     }
 }
